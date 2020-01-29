c04f1e9f57a34b7d5e295e3df267eef11124b05b
Viacheslav_Shiliaev <lass710@gmail.com>
2020-01-29 17:56:32 +0300

    Внедрение миграции

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -4,7 +4,7 @@ create table bookmark (
     id int8 not null,
     file_name varchar(255),
     link varchar(255),
-    text varchar(255),
+    text varchar(2048) not null,
     user_id int8, primary key (id));
 
 create table user_role (
@@ -15,15 +15,15 @@ create table usr (
     id int8 not null,
     activation_code varchar(255),
     active boolean,
-    email varchar(255),
-    password varchar(255),
-    username varchar(255),
+    email varchar(255) not null,
+    password varchar(255) not null,
+    username varchar(255) not null,
     primary key (id));
 
 alter table if exists bookmark
-    add constraint FK3claygmi8gds4yd97ofdtrtnu
+    add constraint bookmark_user_fk
     foreign key (user_id) references usr;
 
 alter table if exists user_role
-    add constraint FKfpm8swft53ulq2hl11yplpr5
+    add constraint user_role_user_fk
     foreign key (user_id) references usr;
